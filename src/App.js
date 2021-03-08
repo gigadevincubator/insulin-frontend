adea432115f9cf79ab7a0a211db7dfaf44eb2707
Constantin Ginga <costelgn@gmail.com>
2021-03-08 17:36:11 +0100

    Add path for Edit Tutorial

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Tutorial from './components/Tutorial';
 import './App.css';
 
 import Dashboard from './components/Dashboard/Dashboard';
+import Edit from './components/Edit/Edit';
 
 function App() {
   return (
     <Switch>
       <Route component={Dashboard} exact path="/" />
       <Route component={Tutorial} path="/tutorials/:id/steps/:stepNumber" />
+      <Route component={Edit} path="/tutorials/:id/languages/:langid/edit" />
     </Switch>
   );
 }
