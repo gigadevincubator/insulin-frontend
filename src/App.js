3e97531f936d1d3b67520fe08c325188c0d01512
Constantin Ginga <costelgn@gmail.com>
2021-03-21 21:31:17 +0100

    Add route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 
 import Dashboard from './components/Dashboard/Dashboard';
 import Edit from './components/Edit/Edit';
+import CreateTutorial from './components/CreateTutorial/CreateTutorial';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route component={Dashboard} exact path="/" />
       <Route component={Tutorial} path="/tutorials/:id/steps/:stepNumber" />
       <Route component={Edit} path="/tutorials/:id/languages/:langid/edit" />
+      <Route component={CreateTutorial} exact path="/create-tutorial" />
     </Switch>
   );
 }
